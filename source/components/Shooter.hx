89e9c8ac9d27513d3b84bb1e0fdfa901ae0db9f6
Erin Gunn <ikonone@gmail.com>
2015-03-29 23:40:04 -0400

    Shooter only ever creates 1 gun now.

--- source/components/Shooter.hx
+++ source/components/Shooter.hx
@@ -20,15 +20,15 @@ class Shooter extends IkComponent
 	
 	public function setGun(type:String, offX:Float, offY:Float):Void
 	{
-		if(gun != null)
-			FlxG.state.remove(gun);
-			
-		gun = new Gun();
-		FlxG.state.add(gun);
+		if(gun == null)
+		{
+			gun = new Gun();
+			FlxG.state.add(gun);
+		}
+
 		gun.setData(GC.GUNS.get(type));
 		gun.setParent(owner);
 		gun.setReleativePos(offX, offY);
-		gun.exists = gun.active = enabled;
 	}
 
 	override public function update():Void
@@ -43,7 +43,7 @@ class Shooter extends IkComponent
 			return enabled;
 
 		if(gun != null)
-			gun.active = gun.exists = enabled;
+			gun.kill();
 
 		return super.setEnabled(enabled);
 	}
